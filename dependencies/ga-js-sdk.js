23f6bc6589d876b1a7ce4bb310241abefe623146
Justin Espedal <justin.espedal@gmail.com>
2023-01-11 23:01:58 +0900

    Update JS SDK to 4.4.5

--- dependencies/ga-js-sdk.js
+++ dependencies/ga-js-sdk.js
@@ -1,7 +1,7 @@
 window.GameAnalytics=window.GameAnalytics||function(){(GameAnalytics.q=GameAnalytics.q||[]).push(arguments)};
 
 var sdkScript = document.createElement('script');
-sdkScript.src = 'http://download.gameanalytics.com/js/GameAnalytics-3.1.2.min.js';
+sdkScript.src = 'http://download.gameanalytics.com/js/GameAnalytics-4.4.5.min.js';
 sdkScript.setAttribute("type", "text/javascript");
 sdkScript.async = true;
 document.head.appendChild(sdkScript);
